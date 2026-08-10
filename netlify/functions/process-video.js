faed8bdb979747e21817748621b36e0638a1c75c
Carl Wright <carlw1059@gmail.com>
2026-08-10 12:01:48 +1000

    debug: add GET diagnostic to process-video function

--- netlify/functions/process-video.js
+++ netlify/functions/process-video.js
@@ -5,6 +5,17 @@ const os = require('os');
 const { createClient } = require('@supabase/supabase-js');
 
 exports.handler = async function(event, context) {
+  if (event.httpMethod === 'GET') {
+    const files = fs.readdirSync(__dirname);
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        __dirname,
+        files,
+        ffmpegExists: fs.existsSync(path.join(__dirname, 'ffmpeg'))
+      })
+    };
+  }
   if (event.httpMethod !== 'POST') {
     return { statusCode: 405, body: JSON.stringify({ error: 'Method not allowed' }) };
   }
